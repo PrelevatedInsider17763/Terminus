d007b70fb00d577029bf3ea8203ddffa1e868e12
PrelevatedInsider22581 <sreeram.sathya2014@outlook.com>
2024-02-19 20:32:50 +0530

    .

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const WebSocket = require('ws');
+const pty = require('node-pty');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  const term = pty.spawn('cmd.exe', [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.USERPROFILE,
+    env: process.env
+  });
+
+  term.on('data', function(data) {
+    ws.send(data.toString());
+  });
+
+  ws.on('message', function(message) {
+    term.write(message);
+  });
+
+  ws.on('close', function() {
+    term.kill();
+  });
+});
