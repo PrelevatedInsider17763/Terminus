3cd3c79db8d99565e8ec53647f5ff26f3d3467fc
MemberMaddenInsiderDev25120 <sreeram.sathya2014@outlook.com>
2024-02-19 20:57:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const pty = require('node-pty');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  const term = pty.spawn('cmd.exe', [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 24,
-    cwd: process.env.USERPROFILE,
-    env: process.env
-  });
+  if (process.platform === "win32") {
+    const term = pty.spawn('cmd.exe', [], {
+      name: 'xterm-color',
+      cols: 80,
+      rows: 24,
+      cwd: process.env.USERPROFILE,
+      env: process.env
+    });
+  } else {
+    const term = pty.spawn('/bin/bash', [], {
+      name: 'xterm-color',
+      cols: 80,
+      rows: 24,
+      cwd: process.env.USERPROFILE,
+      env: process.env
+    });
+  }
 
   term.on('data', function(data) {
     ws.send(data.toString());
